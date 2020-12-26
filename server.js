62d42b6222b38e7bb9ccb325ae79d92abb43ec57
MeetYourCreator <ramizackshamir@icloud.com>
2020-12-26 12:17:58 -0500

    rendering dynamic url for rooms

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const express = require('express')
 //create app variable initialized with running the expresss function; the app variable is initialized as the express function
 const app = express()
-//instantiate a Server based on ExpressJS and pass it the app variable. Thjis allows us to create a server to be used with socket.io
+//instantiate a Server based on ExpressJS and pass it the app variable. This creates a server to be used with socket.io
 const server = require('http').Server(app)
-//import socket.io and pass it the server variable. This way socket.io knows which server we're using and how to interact with that server.
+//import socket.io and pass it the server variable. This way socket.io knows which server is being used and how to interact with that server.
 const io = require('socket.io')(server)
-//specify which port to start up the server on (port 3000; aka localhost)
+//specify which port to start up the server on (port 3000)
 server.listen(3000)
 //line 21 - 22
 const { v4: uuidV4 } = require('uuid')
@@ -18,7 +18,7 @@ app.use(express.static('public'))
 app.get('/', (request, response) => {
   //redirect user to dynamic room, line 24
   // response.redirect(`/${roomId}`)
-  // instead of passing roomId, call the uuidV4 function (declared in line 12) to get a dynamic url; e.g: http://localhost:3000/0e5ce052-8b71-4f7a-a397-f17c3e7d6971
+  // instead of passing roomId, call the uuidV4 function (declared in line 12) to get a dynamic url by appemnding a randfom uuid to localhost:3000, e.g: http://localhost:3000/0e5ce052-8b71-4f7a-a397-f17c3e7d6971
   response.redirect(`/${uuidV4()}`)
 })
 //create route for chat rooms, first argument is the route, second argument is a callback function which takes two parameters, request and response
