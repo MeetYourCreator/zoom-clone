c9351661a2f4d74ea0ac2a1e9b6123960478e760
MeetYourCreator <ramizackshamir@icloud.com>
2020-12-27 10:27:26 -0500

    init Peer server setup

--- public/script.js
+++ public/script.js
@@ -1,9 +1,16 @@
-//create a reference to socket and connect to root path
+//create a reference to socket and pass in root path to connect to root path
 const socket = io('/')
+
+//since I'm using a back-end server (besides the peer server), I have to pass in two  parameters to the new instance of the Peer object: 1. first parameter is the id; pass in a value of undefined because the serve will be generating the userId 2. second paramater is an object with two key-value pairs
+let peer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+})
 
 //pass three arguments to an instance of EventEmitter to the back-end using socket.io: 1. event from server.js, line 35 2. roomID from server.js, line 28 via the romm_id variable from room.ejs, line 7
 socket.emit('join-room', room_id, 39)
 
+//listen for new user connecting; on mnew 'user-connected' fire up callback function which console's thast the the new user (userId) hads connected.
 socket.on('user-connected', userId => {
   console.log(`${userId} has joined the room`)
 })
